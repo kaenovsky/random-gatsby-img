8fe31351c6f772f2db54fc205e8d5c90dc630e81
kaen <kaen@alpogo.com>
2020-11-12 21:53:17 -0300

    - upload project to github

---
+++ src/components/random.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function Random() {
+    return <img src = "https://source.unsplash.com/random/400x200"
+    alt = "a random image from unsplash just loaded here"
+    title = "I'm a component actually" />
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
+import Random from "../components/random"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `white` }}>
+      <Header headerText="what a random pic" />
+      <Random />
+      <p>[ random images from <a href="https://unsplash.com" target="_blank">unsplash.com</a> ]</p>
+      <Header headerText="but it's a component" />
+      <h4> ¯\_(ツ)_/¯ </h4>
+    </div>
+  )
 }
